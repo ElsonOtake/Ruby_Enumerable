f7f1bba484c705703c5a1ae45c788411b3635194
Elson Otake <elsonotake@yahoo.com.br>
2022-08-18 17:57:51 -0300

    Add each method

--- MyList.rb
+++ MyList.rb
@@ -1,7 +1,12 @@
+require_relative('MyEnumerable')
+
 class MyList
-  # include MyEnumerable
+  include MyEnumerable
   def initialize(*list)
     @list = list
+  end
+  def each(&block)
+    @list.each { |a| block.call(a) }
   end
   def size
     @list.length
@@ -14,7 +19,12 @@ class MyList
   end
 end
 
-list = MyList.new(1, 2, 3,4)
+list = MyList.new(1, 2, 3, 4)
 p list.size
 p list.first
 p list.last
+p list.all? {|e| e < 5}
+p list.all? {|e| e > 5}
+p list.any? {|e| e == 2}
+p list.any? {|e| e == 5}
+p list.filter {|e| e.even?}
